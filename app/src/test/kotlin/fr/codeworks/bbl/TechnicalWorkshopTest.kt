a24552ccc8961ecb2af362ce1eaf8c9dc32dee07
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-02-18 11:23:38 +0100

    init commit

---
+++ app/src/test/kotlin/fr/codeworks/bbl/TechnicalWorkshopTest.kt
@@ -0,0 +1,42 @@
+package fr.codeworks.bbl
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.PrintStream
+
+internal class TechnicalWorkshopTest{
+    lateinit var out: PrintStream
+    val filePath = "src/test/resources/init"
+
+    @BeforeEach
+    fun setup(){
+        out = System.out
+        System.setOut(PrintStream(FileOutputStream("$filePath/lead.txt")))
+
+    }
+    @AfterEach
+    fun destroy(){
+        System.setOut(out)
+    }
+
+    @Test
+    @DisplayName("Should make sure the last outputs matches gold")
+    fun shouldRunMain() {
+        val gold = BufferedReader(FileReader("$filePath/gold.txt"))
+        val lead = BufferedReader(FileReader("$filePath/lead.txt"))
+
+        main()
+
+        var line: String?
+        while (gold.readLine().also { line = it } != null) {
+            Assertions.assertThat(line).isEqualTo(lead.readLine())
+        }
+
+    }
+}
